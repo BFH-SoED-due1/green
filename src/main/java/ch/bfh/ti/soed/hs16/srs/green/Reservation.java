722a87ca6943aac78c3692ea8c97528c8b51c3ff
galam1 <marco.galatioto@hotmail.com>
2016-10-20 14:00:57 +0200

    Added initial model.

---
+++ src/main/java/ch/bfh/ti/soed/hs16/srs/green/Reservation.java
@@ -0,0 +1,79 @@
+package project;
+
+import java.util.Date;
+import java.util.Set;
+
+public class Reservation {
+
+	private Date date;
+	// private Time time;
+	private boolean periodic;
+	private Room room;
+	private final User user;
+
+	Reservation(Date date, /* Time time */ Room room, User user) throws Exception {
+
+		if (user == null)
+			throw new Exception("Cannot make a Reservation, User must be needed");
+		else {
+			this.user = user;
+			this.user.addReservationsMadeByUser(this);
+		}
+
+		if (!Room.getRooms().contains(room))
+			throw new Exception("No such Room");
+
+		this.room = room;
+		this.date = date;
+
+		if (isAvailable(room))
+			room.addReservation(this);
+
+		else
+			throw new Exception("Date already booked!");
+
+	}
+
+	// public void setReservation(Room room) throws Exception {
+	// if (isAvailable(room)) {
+	// Reservation newRes = new Reservation(date, room, user);
+	// room.addReservation(this);
+	// user.addReservationsMadeByUser(this);
+	// }
+	//
+	// else
+	// throw new Exception("Room already booked at the desired time");
+	// }
+
+	public Date getDate() {
+		return date;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public boolean isPeriodic() {
+		return periodic;
+	}
+
+	public void setPeriodic(boolean periodic) {
+		this.periodic = periodic;
+	}
+
+	public Room getRoom() {
+		return room;
+	}
+
+	public boolean isAvailable(Room room) {
+		Set<Reservation> reservations = room.getReservations();
+		if (reservations == null)
+			return true;
+		for (Reservation r : reservations)
+			if (r.getDate().equals(date))
+				return false;
+		return true;
+
+	}
+
+}
---
+++ src/main/java/ch/bfh/ti/soed/hs16/srs/green/User.java
@@ -0,0 +1,51 @@
+package project;
+
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+public class User {
+
+	private final String userName;
+	private String department;
+	private final String ahv;
+	private Set<Reservation> bookedReservation;
+
+	User(String userName, String ahv) {
+		this.ahv = ahv;
+		this.userName = userName;
+		bookedReservation = new HashSet<>();
+
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	public String getAhv() {
+		return ahv;
+	}
+
+	public void makeReservation(Date date, Room room) throws Exception {
+		Reservation reserv = new Reservation(date, room, this);
+		bookedReservation.add(reserv);
+	}
+
+	public Set<Reservation> getReservations() {
+		return Collections.unmodifiableSet(bookedReservation);
+	}
+
+	public void addReservationsMadeByUser(Reservation reservation) {
+		bookedReservation.add(reservation);
+	}
+
+}
