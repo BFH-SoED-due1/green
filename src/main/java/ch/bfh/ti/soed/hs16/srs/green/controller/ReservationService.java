b1681c1f2fd4084a2ca8e01c2a0e79bd33835889
wuthv1 <wuthv1@students.bfh.ch>
2016-11-13 20:44:18 +0100

    added ReservationService

---
+++ src/main/java/ch/bfh/ti/soed/hs16/srs/green/controller/ReservationService.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2016 Berner Fachhochschule, Switzerland.
+ *
+ * Project Smart Reservation System.
+ *
+ * Distributable under GPL license. See terms of license at gnu.org.
+ */
+package ch.bfh.ti.soed.hs16.srs.green.controller;
+
+public class ReservationService {
+/*
+	public void deleteReservation(Reservation r) {
+//	-> delete from database or set inactive?
+	}
+
+	public List<Reservation> getReservationsOfTimeSlice(LocalDateTime startime, LocalDateTime endtime) {
+		return null;
+	}
+
+	public Reservation reserve(Customer c, LocalDateTime s, LocalDateTime e, Resource r) {
+//		- Database add new Reservation()
+//		- send confirm email ?
+		return null;
+	}
+
+//	public void changeReservation(Reservation, value to change, newvalue) {
+//
+//	- set()
+//
+//	}
+
+	public void reserveRequest(Customer c, LocalDateTime s, LocalDateTime e, Resource r) {
+//	-> if all ok: reserve(c, s, e, r)
+//	   if not: reject /exception
+
+	}
+*/
+}
