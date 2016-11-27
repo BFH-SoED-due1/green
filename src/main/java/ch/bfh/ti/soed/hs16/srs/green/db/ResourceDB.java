02883e91ee725e1859c2b467f05cecca443edf44
masa <masa@192.168.1.3>
2016-11-27 17:15:12 +0100

    Created class ResourceDB for connecting to resource db (made together by all teammembers)

---
+++ src/main/java/ch/bfh/ti/soed/hs16/srs/green/db/ResourceDB.java
@@ -0,0 +1,101 @@
+package ch.bfh.ti.soed.hs16.srs.green.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.HashSet;
+import java.util.Set;
+
+import ch.bfh.ti.soed.hs16.srs.green.model.Resource;
+
+public class ResourceDB {
+
+	private static Connection c = null;
+	private static Statement stmt = null;
+	private static int ID;
+
+	public static void addResource(String roomName, String locatoin, int size) throws Exception {
+
+		try {
+			Class.forName("org.sqlite.JDBC");
+			System.out.println("FORNAME");
+			c = DriverManager.getConnection("jdbc:sqlite:resource.db");
+			System.out.println("DRIVERMANAGER");
+
+			stmt = c.createStatement();
+
+			ResultSet rs = stmt.executeQuery("SELECT ID FROM CUSTOMER;");
+
+			while (rs.next())
+				ID = rs.getInt("id") + 1;
+
+			rs.close();
+
+			String sql = "INSERT INTO RESOURCES (ID,ROOMNAME,LOCATION,SIZE) " + "VALUES (" + ID++ + ", '" + roomName
+					+ "', '" + locatoin + "', '" + size + "');";
+			System.out.println("CREATESTATEMENT");
+			stmt.executeUpdate(sql);
+			System.out.println("EXECUTEUPDATE");
+
+			stmt.close();
+			System.out.println("CLOSE");
+			c.close();
+			System.out.println("CLOSE");
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+	}
+
+	public static int getAmountRooms() {
+		try {
+			Class.forName("org.sqlite.JDBC");
+			c = DriverManager.getConnection("jdbc:sqlite:resource.db");
+			c.setAutoCommit(false);
+			stmt = c.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT ID FROM RESOURCES;");
+
+			while (rs.next())
+				ID = rs.getInt("id");
+
+			rs.close();
+			stmt.close();
+			c.close();
+		} catch (Exception e) {
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+
+		}
+		return ID;
+	}
+
+	public static Set<Resource> getResources() {
+		c = null;
+		stmt = null;
+		Set<Resource> resources = new HashSet<>();
+		try {
+			Class.forName("org.sqlite.JDBC");
+			c = DriverManager.getConnection("jdbc:sqlite:resource.db");
+			c.setAutoCommit(false);
+			stmt = c.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT ROOMNAME, LOCATION, SIZE FROM RESOURCES;");
+
+			while (rs.next()) {
+				String roomName = rs.getString("roomName");
+				String location = rs.getString("location");
+				int size = rs.getInt("size");
+				resources.add(new Resource(roomName, size, location));
+			}
+			rs.close();
+			stmt.close();
+			c.close();
+		} catch (Exception e) {
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+
+		}
+
+		return resources;
+	}
+
+}
