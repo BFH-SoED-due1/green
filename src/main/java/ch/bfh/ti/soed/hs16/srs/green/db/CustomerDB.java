2c00fb2a9a6a478f09d4b5be16ae4946f083de62
masa <masa@192.168.1.6>
2016-12-24 20:55:48 +0100

    javadoc

--- src/main/java/ch/bfh/ti/soed/hs16/srs/green/db/CustomerDB.java
+++ src/main/java/ch/bfh/ti/soed/hs16/srs/green/db/CustomerDB.java
@@ -17,22 +17,46 @@ import java.util.Set;
 import ch.bfh.ti.soed.hs16.srs.green.model.Customer;
 import ch.bfh.ti.soed.hs16.srs.green.model.Role;
 
+/**
+ * A class which represents the customer table in the srs.db. Class is only used
+ * by the class MyUIControllers.
+ * 
+ * @author team-green
+ * @version 1.4, 18.12.16
+ */
 public class CustomerDB {
 
 	private static Connection c = null;
 	private static Statement stmt = null;
 
+	/**
+	 * A Method which adds a customer to the table customer in the srs.db
+	 * 
+	 * @param userName
+	 *            takes the username from the UI-TextField username.
+	 * @param pw
+	 *            takes the password from the UI-TextField password.
+	 * @throws Exception
+	 */
 	public static void registerCustomer(String userName, String pw, Role x) throws Exception {
 
 		Class.forName("org.sqlite.JDBC");
 		c = DriverManager.getConnection("jdbc:sqlite:srs.db");
-		
-		String sql = "INSERT INTO Customer (USERNAME,PW,RIGHTS) " + "VALUES ('" + userName + "', '" + pw + "', '"+x.toString()+"');";
+
+		String sql = "INSERT INTO Customer (USERNAME,PW,RIGHTS) " + "VALUES ('" + userName + "', '" + pw + "', '"
+				+ x.toString() + "');";
 		c.createStatement().executeUpdate(sql);
 
 		c.close();
 	}
 
+	/**
+	 * Returns all customers in the customer table of srs.db.
+	 * 
+	 * @return a set of all customers.
+	 * @throws Exception
+	 * @see Customer
+	 */
 	public static Set<Customer> getCustomers() throws Exception {
 
 		c = null;
@@ -41,7 +65,7 @@ public class CustomerDB {
 
 		Class.forName("org.sqlite.JDBC");
 		c = DriverManager.getConnection("jdbc:sqlite:srs.db");
-	//	c.setAutoCommit(false);
+		// c.setAutoCommit(false);
 		stmt = c.createStatement();
 
 		ResultSet rs = stmt.executeQuery("SELECT USERNAME, PW, RIGHTS FROM CUSTOMER;");
@@ -50,8 +74,8 @@ public class CustomerDB {
 			String userName = rs.getString("username");
 
 			String pw = rs.getString("pw");
-			
-			customers.add(new Customer(userName, pw,Role.valueOf(rs.getString("rights"))));
+
+			customers.add(new Customer(userName, pw, Role.valueOf(rs.getString("rights"))));
 		}
 
 		rs.close();
