63cecdeb58392a200eb39b11370383fe7e6af8e2
masa <masa@192.168.1.6>
2016-12-24 21:00:34 +0100

    javadoc

--- src/main/java/ch/bfh/ti/soed/hs16/srs/green/model/Role.java
+++ src/main/java/ch/bfh/ti/soed/hs16/srs/green/model/Role.java
@@ -1,6 +1,11 @@
 package ch.bfh.ti.soed.hs16.srs.green.model;
 
+/**
+ * Enum used by class Customer, a room manager has more rights then a customer.
+ * 
+ * @author team-green
+ * @see Customer
+ */
 public enum Role {
-	CUSTOMER,
-	ROOMMANAGER
+	CUSTOMER, ROOMMANAGER
 }
