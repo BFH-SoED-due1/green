3878a8e225f968dcdcf10fbe84547345dc85dc89
masa <masa@192.168.1.6>
2016-12-24 20:59:25 +0100

    javadoc

--- src/main/java/ch/bfh/ti/soed/hs16/srs/green/db/ResourceDB.java
+++ src/main/java/ch/bfh/ti/soed/hs16/srs/green/db/ResourceDB.java
@@ -16,11 +16,29 @@ import java.util.Set;
 
 import ch.bfh.ti.soed.hs16.srs.green.model.Resource;
 
+/**
+ * A class which represents the resources table in the srs.db. Class is only
+ * used by the class MyUIControllers.
+ * 
+ * @author team-green
+ * @version 1.4, 18.12.16
+ */
 public class ResourceDB {
 
 	private static Connection c = null;
 	private static Statement stmt = null;
 
+	/**
+	 * Adds a new Resource/Room to the resources table in the srs.db.
+	 * 
+	 * @param roomName
+	 *            the specified room name.
+	 * @param locatoin
+	 *            the specified location of the room.
+	 * @param size
+	 *            the size of the room.
+	 * @throws Throwable
+	 */
 	public static void addResource(String roomName, String locatoin, int size) throws Throwable {
 
 		Class.forName("org.sqlite.JDBC");
@@ -34,19 +52,36 @@ public class ResourceDB {
 		c.close();
 
 	}
+
+	/**
+	 * Method removes a specific resource.
+	 * 
+	 * @param roomName
+	 *            specific room name
+	 * @param locatoin
+	 *            specific location
+	 * @throws Throwable
+	 * @see Resource
+	 */
 	public static void removeResource(String roomName, String locatoin) throws Throwable {
 
 		Class.forName("org.sqlite.JDBC");
 		c = DriverManager.getConnection("jdbc:sqlite:srs.db");
 		stmt = c.createStatement();
 
-		String sql = "DELETE FROM RESOURCES WHERE ROOMNAME= '" + roomName + "' AND LOCATION= '" + locatoin+ "';";
+		String sql = "DELETE FROM RESOURCES WHERE ROOMNAME= '" + roomName + "' AND LOCATION= '" + locatoin + "';";
 		stmt.executeUpdate(sql);
 		stmt.close();
 		c.close();
 
 	}
 
+	/**
+	 * Returns the amount of rooms of the resources table.
+	 * 
+	 * @return amount of rooms.
+	 * @throws Throwable
+	 */
 	public static int getAmountRooms() throws Throwable {
 		Class.forName("org.sqlite.JDBC");
 		c = DriverManager.getConnection("jdbc:sqlite:srs.db");
@@ -59,6 +94,13 @@ public class ResourceDB {
 		return res;
 	}
 
+	/**
+	 * Returns all resources/rooms of the resources-table.
+	 * 
+	 * @return a set of resource.
+	 * @throws Throwable
+	 * @see Resource
+	 */
 	public static Set<Resource> getResources() throws Throwable {
 		c = null;
 		stmt = null;
