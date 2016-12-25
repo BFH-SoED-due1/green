47b39f787a40312fbdcaf35c678a5760bc1d31b3
masa <masa@192.168.1.6>
2016-12-25 17:58:18 +0100

    javadoc

--- src/main/java/ch/bfh/ti/soed/hs16/srs/green/model/Reservation.java
+++ src/main/java/ch/bfh/ti/soed/hs16/srs/green/model/Reservation.java
@@ -9,6 +9,11 @@ package ch.bfh.ti.soed.hs16.srs.green.model;
 
 import java.time.LocalDateTime;
 
+/**
+ * Class Reservation which handles one reservation and which has all data of one
+ * (this) reservation.
+ * @author team-green
+ */
 public class Reservation {
 
 	private LocalDateTime startTime, endTime;
@@ -16,6 +21,14 @@ public class Reservation {
 	private Resource resource;
 	private Customer customer;
 
+	/**
+	 * Constructor which initialize all necessary data for a reservation.
+	 * 
+	 * @param startTime
+	 * @param endTime
+	 * @param resource
+	 * @param customer
+	 */
 	public Reservation(LocalDateTime startTime, LocalDateTime endTime, Resource resource, Customer customer) {
 		this.startTime = startTime;
 		this.endTime = endTime;
@@ -24,34 +37,65 @@ public class Reservation {
 
 	}
 
+	/**
+	 * Method returns the Customer which made this Reservation.
+	 * @return
+	 */
 	public Customer getCustomer() {
 		return customer;
 	}
 
+	/**
+	 * Returns the Date and the End-Time of this Reservation.
+	 * @return
+	 */
 	public LocalDateTime getEndTime() {
 		return endTime;
 	}
 
+	/**
+	 * returns the Resource(Room) of this Reservation.
+	 * @return
+	 */
 	public Resource getResource() {
 		return resource;
 	}
 
+	/**
+	 * Returns the Date and the Start-Time of this Reservation.
+	 * @return
+	 */
 	public LocalDateTime getStartTime() {
 		return startTime;
 	}
-
+	/**
+	 * Sets the given Customer to this reservation.
+	 * @param endTime
+	 */
 	public void setCustomer(Customer customer) {
 		this.customer = customer;
 	}
 
+	/**
+	 * Sets the end Time to this reservation.
+	 * @param endTime
+	 */
 	public void setEndTime(LocalDateTime endTime) {
 		this.endTime = endTime;
 	}
 
+	/**
+	 * Sets the given Resource to this Reservation.
+	 * @param resource
+	 */
 	public void setResource(Resource resource) {
 		this.resource = resource;
 	}
 
+	/**
+	 * Sets the given StartTime to this Reservation.
+	 * @param startTime
+	 */
 	public void setStartTime(LocalDateTime startTime) {
 		this.startTime = startTime;
 	}
