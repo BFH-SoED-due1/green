af3d2d67f7f8232b176d2b96ae6c7e39bae1ccf2
masa <masa@192.168.1.6>
2016-12-25 14:46:24 +0100

    new Class to connecto to the DB

---
+++ src/main/java/ch/bfh/ti/soed/hs16/srs/green/db/DBConnector.java
@@ -0,0 +1,41 @@
+package ch.bfh.ti.soed.hs16.srs.green.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnector {
+
+	public static Connection c = null;
+
+	/**
+	 * Method, which will do the connection to the database.
+	 * 
+	 * @throws Throwable
+	 */
+	public static void connectDB() throws Throwable {
+		Class.forName("org.sqlite.JDBC");
+		c = DriverManager.getConnection("jdbc:sqlite:srs.db");
+	}
+
+	/**
+	 * Method, which makes the disconnect to the database.
+	 * 
+	 * @throws Throwable
+	 */
+	public static void disconnectDB() throws Throwable {
+		c.close();
+	}
+
+	/**
+	 * Deletes all the content of the tables in the srs.db - mostly neede for
+	 * the test classes.
+	 * 
+	 * @throws Throwable
+	 */
+	public static void delteContentOfTabels() throws Throwable {
+		c.createStatement().executeUpdate("delete from reservations;");
+		c.createStatement().executeUpdate("delete from customer;");
+		c.createStatement().executeUpdate("delete from resources;");
+	}
+
+}
