2dd734af277e73a6ce4cf128699bc8e40f818f72
masa <masa@192.168.1.6>
2016-12-25 18:01:51 +0100

    javadoc

--- src/main/java/ch/bfh/ti/soed/hs16/srs/green/model/Resource.java
+++ src/main/java/ch/bfh/ti/soed/hs16/srs/green/model/Resource.java
@@ -7,56 +7,75 @@
  */
 package ch.bfh.ti.soed.hs16.srs.green.model;
 
-import java.rmi.server.UID;
-
+/**
+ * Class Resource which has all information of a specific Resource.
+ * @author team-green
+ */
 public class Resource {
 
 	private String name, location;
 	private int size;
-	private UID UID;
 
+	/**
+	 * Constructor which initialize all data for a Resource.
+	 * @param name
+	 * @param size
+	 * @param location
+	 */
 	public Resource(String name, int size, String location) {
 
 		this.name = name;
 		this.size = size;
 		this.location = location;
-		UID = new UID();
+
 	}
 
+	/**
+	 * returns the location of this resource.
+	 * @return
+	 */
 	public String getLocation() {
 		return location;
 	}
 
+	/**
+	 * returns the name of this Resource.
+	 * @return
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * returns the size of this Resource.
+	 * @return
+	 */
 	public int getSize() {
 		return size;
 	}
 
-	public UID getUID() {
-		return UID;
-	}
-
+	/**
+	 * sets the specific location to this Resource.
+	 * @param location
+	 */
 	public void setLocation(String location) {
 		this.location = location;
 	}
 
+	/**
+	 * sets the specific name to this Resource.
+	 * @param name
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	/**
+	 * sets the specific size to this Resource.
+	 * @param size
+	 */
 	public void setSize(int size) {
 		this.size = size;
 	}
-
-	// public void add() {
-	// add resource to database
-	// }
-
-	// public void remove() {
-	// remove resource to database and delete object
-	// }
 
 }
